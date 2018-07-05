8bad2a6826b7ef0a6bbd90ce7127a4efffa0ad51
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-07-05 17:04:55 -0400

    trying to do better screen wrapping with overlapBox2D

--- Kid Icarus/Assets/Scripts/Player/PlayerMovement.cs
+++ Kid Icarus/Assets/Scripts/Player/PlayerMovement.cs
@@ -76,7 +76,6 @@ public class PlayerMovement : MonoBehaviour
 			DoTerminalVelocities();
 
 			CheckFlipSprite();
-			ScreenWrapping();
 		}
 
 		if (Input.GetKeyDown(KeyCode.Escape))
@@ -91,6 +90,7 @@ public class PlayerMovement : MonoBehaviour
 		// only allow movement if we're alive
 		if (refPlayerCollision.isDead == false)
 		{
+			ScreenWrapping();
 			DoMovement();
 		}
 	}
@@ -231,14 +231,68 @@ public class PlayerMovement : MonoBehaviour
 
 	private void ScreenWrapping()
 	{
+		/*if (transform.position.x > 15f)
+		{
+			CheckWrapCollision(0.0f);
+			{
+				PushAway(true);
+				return;
+			}
+		}
+
+		if (transform.position.x < 0.0f)
+		{
+			if (CheckWrapCollision(15.0f) == true)
+			{
+				PushAway(false);
+				return;
+			}
+		}*/
+
+		// if we're too far off the right side
 		if (transform.position.x > 15.75f)
 		{
+			// check for walls on the opposite side
 			transform.position = new Vector2(-0.5f, transform.position.y);
 		}
 
-		if (transform.position.x < -0.75f)
+		// if we're too far off the left side
+		if (transform.position.x < -0.75f && !CheckWrapCollision(0))
 		{
+			// check for walls on the opposite side
 			transform.position = new Vector2(15.5f, transform.position.y);
+		}
+	}
+
+	private bool CheckWrapCollision(float xPos)
+	{
+		/*if (isCrouching == true)
+		{
+			if (Physics2D.OverlapBox(new Vector2(xPos, transform.position.y) + (Vector2)crouchedCollider.bounds.center, crouchedCollider.bounds.extents * 0.0f, 0, groundMask))
+			{
+				return true;
+			}
+		}
+		else
+		{
+			if (Physics2D.OverlapBox(new Vector2(xPos, transform.position.y) + (Vector2)defaultCollider.bounds.center, defaultCollider.bounds.extents * 0.75f, 0, groundMask))
+			{
+				return true;
+			}
+		}*/
+		return false;
+	}
+
+	private void PushAway(bool fromRightSide)
+	{
+		// push Pit horizontally according to the multiplier
+		if (fromRightSide == true)
+		{
+			transform.position = new Vector2(15.0f, transform.position.y);
+		}
+		else
+		{
+			transform.position = new Vector2(-0.0f, transform.position.y);
 		}
 	}
 }
