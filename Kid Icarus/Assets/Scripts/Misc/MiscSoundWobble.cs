23babe1c89733eba236ec0050ddd8686d45f3474
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2018-09-30 13:31:47 -0400

    whoops I forgot the meta files

---
+++ Kid Icarus/Assets/Scripts/Misc/MiscSoundWobble.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiscSoundWobble : MonoBehaviour
+{
+   [Header("Pitch wobble values")]
+   [Range(-3.0f, 3.0f)]
+   public float high;
+   [Range(-3.0f, 3.0f)]
+   public float low;
+   [Range(0.0f, 1.0f)]
+   public float lerpSpeed;
+
+   private float switchThreshold = 0.1f;
+   private bool goingUp = false;
+
+   [Header("Affect this audio source")]
+   public AudioSource source;
+	
+	void Update ()
+   {
+      if (goingUp == true)
+      {
+         if (source.pitch < high - switchThreshold)
+         {
+            source.pitch = Mathf.Lerp(source.pitch, high, lerpSpeed);
+         }
+         else
+         {
+            goingUp = false;
+         }
+      }
+
+      if (goingUp == false)
+      {
+         if (source.pitch > low + switchThreshold)
+         {
+            source.pitch = Mathf.Lerp(source.pitch, low, lerpSpeed);
+         }
+         else
+         {
+            goingUp = true;
+         }
+      }
+	}
+}
