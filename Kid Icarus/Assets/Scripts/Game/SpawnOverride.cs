b47e9184e79919a61afe71c8a228b6fe4f20ec7a
matthewroy01 <matthew.roy01@mymail.champlain.edu>
2019-05-27 00:43:28 -0400

    added SpawnOverride to ease the player in with less enemies at first

---
+++ Kid Icarus/Assets/Scripts/Game/SpawnOverride.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOverride : MonoBehaviour
+{
+    public bool doOverride;
+    public SpOv[] overrides;
+    public int currentOverrideID = 0;
+
+    private InfiniteGenerator refInfiniteGenerator;
+    private PlayerCollision refPlayerCollision;
+
+    private void Start()
+    {
+        refInfiniteGenerator = FindObjectOfType<InfiniteGenerator>();
+        refPlayerCollision = FindObjectOfType<PlayerCollision>();
+
+        if (doOverride)
+        {
+            Debug.LogWarning("SpawnOverride script is overriding the number of enemies to spawn in the Infinite Generator.");
+        }
+    }
+
+    private void Update()
+    {
+        if (doOverride && currentOverrideID != overrides.Length)
+        {
+            OverrideSpawns();
+        }
+    }
+
+    private void OverrideSpawns()
+    {
+        if (refPlayerCollision.getCurrentMeters() >= overrides[currentOverrideID].meters)
+        {
+            refInfiniteGenerator.enemiesToSpawn = overrides[currentOverrideID].numToSpawn;
+            currentOverrideID++;
+        }
+    }
+}
+
+[System.Serializable]
+public struct SpOv
+{
+    public int meters;
+    public int numToSpawn;
+}
